a22fa653ab656ea0bf436c7e6e58b945450c7ce8
anju3458 <andreasjugueta1@gmail.com>
2026-08-09 11:48:06 +0200

    Info & Kontakt hinzugefügt
    - Infoseite
    - Kontakt/Impressum

---
+++ app/schülerlabore/JS/info.js
@@ -0,0 +1,20 @@
+import { createHeader } from "../../workshop_creator/JS/HEADER/header.mjs";
+
+const BODY = document.body;
+const HEADER = createHeader();
+
+const navLinks = HEADER.querySelectorAll("a");
+
+navLinks.forEach(link => {
+    if (link.textContent.includes("Home")) {
+        link.href = "./home.html";
+    } 
+    else if (link.textContent.includes("Infos")) {
+        link.href = "./info.html"; // This links to itself, acting as a refresh!
+    } 
+    else if (link.textContent.includes("Kontakt")) {
+        link.href = "./kontakt.html"; 
+    }
+});
+
+BODY.prepend(HEADER);
